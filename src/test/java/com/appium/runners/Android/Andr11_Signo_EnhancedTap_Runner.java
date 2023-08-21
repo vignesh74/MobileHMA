deb73f2a0668c4ca0c3d09e53bc49a57c1950185
Akkayasamy, Rajasekar <rajasekar.akkayasamy@hidglobal.com>
2023-08-21 13:23:03 +0530

    WF tag added for android execution

--- src/test/java/com/appium/runners/Android/Andr11_Signo_EnhancedTap_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr11_Signo_EnhancedTap_Runner.java
@@ -14,8 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@Android11_10_Signo_EnhancedTap_WF_6")
-//        , tags = "@Android11_Signo_EnhancedTap")
+        , tags = "@Android11_Signo_EnhancedTap")
 
 public class Andr11_Signo_EnhancedTap_Runner extends BaseTest {
 
