c9720656e4b9024ca54d28de2084f4bd7b9fd866
Raji <raji.johnpeter@hidglobal.com>
2023-08-29 17:16:05 +0530

    Updating Runner and testng xml

--- src/test/java/com/appium/runners/iOS/iOS_Settings_Runner.java
+++ src/test/java/com/appium/runners/iOS/iOS_Settings_Runner.java
@@ -15,7 +15,7 @@ import io.cucumber.testng.CucumberOptions;
         , dryRun = false
         , monochrome = true
         //, tags = "@iOS_Settings_WF3")
-        , tags = "@iOS_Settings_WF")
+        , tags = "@iOS_Settings_WF2")
 
 public class iOS_Settings_Runner extends BaseTest {
 }
