20abf5c5a8eed81a718b0e4cee171ea18345b22a
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2023-10-18 19:52:41 +0530

    Android11_10_RevE_Tap_WF_8 execution

--- src/test/java/com/appium/runners/Android/Andr11_RevE_Tap_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr11_RevE_Tap_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@Android11_10_RevE_Tap_WF_7")
+        , tags = "@Android11_10_RevE_Tap_WF_8")
 //        , tags = "@Android11_RevE_Tap")
 //        , tags = "@Android11_RevE")
 
