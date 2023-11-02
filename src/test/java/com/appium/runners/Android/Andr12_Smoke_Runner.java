5965492d21bdbd4ec224e667d1d9ac4fd85f04c0
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2023-11-02 12:20:16 +0530

    to execute Android11_10_Signo_EnhancedTap_WF_3

--- src/test/java/com/appium/runners/Android/Andr12_Smoke_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr12_Smoke_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@ANDR_SmokeRun10")
+        , tags = "@Android11_10_Signo_EnhancedTap_WF_3")
 
 public class Andr12_Smoke_Runner extends BaseTest {
 
