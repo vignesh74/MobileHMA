02de9c3027b7204d3bd185927a8be6c789b03725
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2023-11-16 12:03:56 +0530

    Executing Android12_RevE_TwistGo in jenkins

--- src/test/java/com/appium/runners/Android/Andr12_RevE_Tap_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr12_RevE_Tap_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@Android12_DE_Tap")
+        , tags = "@Android12_RevE_TwistGo")
 
 public class Andr12_RevE_Tap_Runner extends BaseTest {
 
