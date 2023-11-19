5b60153e04e6468c5d859469aa6130183212a4b4
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2023-11-19 10:20:13 +0530

    executing all the device enforcement TC

--- src/test/java/com/appium/runners/Android/Andr12_RevE_Tap_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr12_RevE_Tap_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@Android12_RevE_Tap")
+        , tags = "@Android12_DE")
 
 public class Andr12_RevE_Tap_Runner extends BaseTest {
 
