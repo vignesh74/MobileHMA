2a7c9191bfe211958416dc9709645aa949459a5c
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2023-11-29 15:58:59 +0530

    Android12_RevE_TwistGo execution

--- src/test/java/com/appium/runners/Android/Andr12_RevTwistAndGo_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr12_RevTwistAndGo_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@Android12_RevE_TwistGo_WF_1")
+        , tags = "@Android12_RevE_TwistGo")
 
 public class Andr12_RevTwistAndGo_Runner extends BaseTest {
 
