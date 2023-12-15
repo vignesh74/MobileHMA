fec4baf4d5ac5c3f8a5524012eff027e1f14a86d
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2023-12-15 15:18:23 +0530

    updated the activity msg in log

--- src/test/java/com/appium/stepDefinition/AndrStepDef.java
+++ src/test/java/com/appium/stepDefinition/AndrStepDef.java
@@ -349,8 +349,7 @@ public class AndrStepDef extends BasePage {
 
                 Assert.assertTrue(mobileIDScreen.verifySuccessIcon());
                 Assert.assertEquals(mobileIDScreen.verifyDate(), strDate);
-//                Assert.assertEquals(mobileIDScreen.getSuccessMessage().toLowerCase(), strMessage.toLowerCase());
-                Assert.assertEquals(mobileIDScreen.getTxtMessage().getText().toLowerCase(),strMessage.toLowerCase());
+                Assert.assertEquals(mobileIDScreen.getSuccessMessage().toLowerCase(), strMessage.toLowerCase());
                 Assert.assertEquals(mobileIDScreen.getActionName().toLowerCase(), strActionName.toLowerCase());
                 Assert.assertEquals(mobileIDScreen.getReaderName().toLowerCase(), strReaderName.toLowerCase());
                 Assert.assertEquals(mobileIDScreen.getMobileIDRead().toLowerCase(), strMobileRead.toLowerCase());
@@ -399,8 +398,6 @@ public class AndrStepDef extends BasePage {
     @Then("Robotic arms log {string} is displayed for android device")
     public void roboticArmsLogIsDisplayed_Andr(String strRoboticLog) {
         TestUtils.log().info("Robotic arms: " + armLogs.first() + " is Equal to " + strRoboticLog);
-        TestUtils.log().info("armLogs.first() "+armLogs.first());
-        TestUtils.log().info("strRoboticLog "+strRoboticLog);
         Assert.assertTrue(armLogs.first().toLowerCase().contains(strRoboticLog.toLowerCase()));
 
     }
