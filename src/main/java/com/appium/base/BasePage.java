77c08a108f0ac6edc272ec659380b06365ccccde
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2024-02-09 18:40:19 +0530

    to identify the adbpath checking again 2

--- src/main/java/com/appium/base/BasePage.java
+++ src/main/java/com/appium/base/BasePage.java
@@ -1418,7 +1418,7 @@ public class BasePage {
     public String getADBPath() {
         TestUtils.log().info("Entered &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
         String androidHome = System.getenv("ANDROID_HOME");
-        TestUtils.log().info("ADB executable not found in androidHome" + androidHome);
+        TestUtils.log().info("%%%%%%%%%%%%%%%%%%%%%%%%%%%" + androidHome);
         if (androidHome != null && !androidHome.isEmpty()) {
             File platformToolsDir = new File(androidHome, "platform-tools");
             if (platformToolsDir.exists()) {
