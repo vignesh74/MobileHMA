a46025634d780d83f8c99d9cb7222e1b7e67168c
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2024-02-19 09:30:35 +0530

    added the TG xml & runner file for device enforcement

---
+++ src/test/java/com/appium/runners/Android/Andr12_DeviceEnforcement_TwistAndGo_Runner.java
@@ -0,0 +1,24 @@
+package com.appium.runners.Android;
+
+import com.appium.base.BaseTest;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = { "pretty"
+                , "html:target/cucumber"
+                , "summary"
+                , "html:target/cucumber-reports/cucumber-html-reports/report.html"
+                , "json:target/cucumber-reports/cucumber-json-report/cucumber.json"
+                , "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }
+        , features = "src/test/resources/features/Android"
+        , glue = "com.appium.stepDefinition"
+        , dryRun = false
+        , monochrome = true
+        , tags = "@Android12_DE_TwistGo")
+
+public class Andr12_DeviceEnforcement_TwistAndGo_Runner extends BaseTest {
+
+
+}
+
+
