978403881ece777ea44b15e330dfb1b46b04003a
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2024-07-16 11:56:04 +0530

    ANDR_Settings_12_WF2 exec

--- src/test/java/com/appium/runners/Android/Andr12_Smoke_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr12_Smoke_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@ANDR_Settings_12_WF1")
+        , tags = "@ANDR_Settings_12_WF2")
 
 public class Andr12_Smoke_Runner extends BaseTest {
 
