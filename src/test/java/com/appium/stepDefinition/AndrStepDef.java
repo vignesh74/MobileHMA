1835d1272f4723070771ac72516ad3006be12bd5
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2024-07-22 11:24:18 +0530

    shifted the method as previous

--- src/test/java/com/appium/stepDefinition/AndrStepDef.java
+++ src/test/java/com/appium/stepDefinition/AndrStepDef.java
@@ -239,7 +239,11 @@ public class AndrStepDef extends BasePage {
 
     @When("Navigate to Settings and App Preferences screen in android device")
     public void navigateToAppPreferencesScreen_Andr() {
-        mobileIDScreen.navigateToAppPreferencesScreen_Andr();
+        mobileIDScreen.clickOnSettingsTab();
+        TestUtils.log().info("Settings screen is displayed");
+        waitForVisibility(settingsScreen.getTxtAppPreference());
+        isElementVisible(settingsScreen.getTxtAppPreference());
+        settingsScreen.clickOnAppPreferences();
     }
 
     @When("Select the usage state as {string} in android device")
