56a78bf6daf8c125747051c1cea2ef5e9b086e10
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2024-07-30 18:04:30 +0530

    changed the device

--- src/test/java/com/appium/runners/Android/Andr12_RevTwistAndGo_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr12_RevTwistAndGo_Runner.java
@@ -16,6 +16,7 @@ import io.cucumber.testng.CucumberOptions;
         , monochrome = true
         , tags = "@Android12_RevE_TwistGo")
 
+
 public class Andr12_RevTwistAndGo_Runner extends BaseTest {
 
 }
