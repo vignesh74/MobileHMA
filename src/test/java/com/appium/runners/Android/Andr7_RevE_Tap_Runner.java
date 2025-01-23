1a02e258146a96e609e0df735b890f34552d506c
vigneshrajesh <vignesh.rajesh@hidglobal.com>
2025-01-23 19:53:16 +0530

    one TC  exec

--- src/test/java/com/appium/runners/Android/Andr7_RevE_Tap_Runner.java
+++ src/test/java/com/appium/runners/Android/Andr7_RevE_Tap_Runner.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.CucumberOptions;
         , glue = "com.appium.stepDefinition"
         , dryRun = false
         , monochrome = true
-        , tags = "@Android9_8_7_RevE_Tap_WF_1")
+        , tags = "@Android12_RevE_Tap_WF_13")
 
 public class Andr7_RevE_Tap_Runner extends BaseTest {
 
